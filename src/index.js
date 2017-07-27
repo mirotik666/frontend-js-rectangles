a0886099b9ab656986430457e4ae30b04f2a623b
Galina <tris151316@gmail.com>
2017-07-27 22:47:45 +0300

    V1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-﻿
+﻿'use strict';
+
+function areIntersected(rectangleA, rectangleB) {
+
+    function getBottomSide(rectangle) {
+        return rectangle.top + rectangle.height;
+    }
+
+    function getRightSide(rectangle) {
+        return rectangle.left + rectangle.width;
+    }
+
+    let intersectedAxisX;
+    let intersectedAxisY;
+
+    if (rectangleA.top < getBottomSide(rectangleB) && getBottomSide(rectangleA) > rectangleB.top) {
+        intersectedAxisY = true;
+    }
+    if (rectangleA.left < getRightSide(rectangleB) && getRightSide(rectangleA) > rectangleB.left) {
+        intersectedAxisX = true;
+    }
+    if (intersectedAxisX & intersectedAxisY) {
+        return true;
+    }
+
+    return false;
+}
+
+
+
+function filterVisible(rectangleA, array) {
+    return array.filter(function(element) {
+        if (element.width !== 0 && element.height !== 0) {
+            return areIntersected(rectangleA, element);
+        } else {
+            return false;
+        }
+    });
+}
+
+console.log(filterVisible);
